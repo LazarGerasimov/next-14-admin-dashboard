acb3c2dbfef44795a3955490b2ba63f27499122e
Laz <lazgerasimov@gmail.com>
2023-11-22 15:31:04 +0200

    Add component scaffolds for: - card - chart - rightbar - transactions

---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,11 @@
+import styles from './rightbar.module.css';
+
+const Rightbar = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default Rightbar
