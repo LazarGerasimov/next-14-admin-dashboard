3846c3fa86446e6da00a9b133ec93776f6ac40b6
Laz <lazgerasimov@gmail.com>
2023-11-23 11:16:04 +0200

    add cards dummy data and styles

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,9 +1,17 @@
+import { MdSupervisedUserCircle } from 'react-icons/md';
 import styles from './card.module.css';
 
 const Card = () => {
     return (
         <div className={styles.container}>
-
+            <MdSupervisedUserCircle size={24} />
+            <div className={styles.texts}>
+                <span className={styles.title}>Total Users</span>
+                <span className={styles.number}>12,204</span>
+                <span className={styles.details}>
+                    <span className={styles.positive}>14%</span> more than previous week
+                </span>
+            </div>
         </div>
     )
 }
