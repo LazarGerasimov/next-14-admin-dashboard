d8ff4b610027111eb78fe415503f72b1021a366c
Laz <lazgerasimov@gmail.com>
2023-11-24 17:47:08 +0200

    create the following: - searchbar component - users page scaffold - products comp scaffold

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Search from '@/app/ui/dashboard/search/search';
+import styles from '../../ui/dashboard/users/users.module.css';
 
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user.."/>
+      </div>
+      <table className={styles.table}>
+
+      </table>
+    </div>
   )
 }
 
