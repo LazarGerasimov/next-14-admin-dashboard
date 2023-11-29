c9f15ffdcc263daa795335d931ab1bf1cbf42368
Laz <lazgerasimov@gmail.com>
2023-11-29 09:36:33 +0200

    create mongodb connection utility

---
+++ app/lib/utils.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+export const connectToDB = async () => {
+
+    const connection = {}
+
+    try {
+        if (connection.isConnected) return;
+        const db = await mongoose.connect(`${process.env.NEXT_MONGO_DB_CONNECTION_STRING}`);
+        connection.isConnected = db.connection[0].readyState
+    } catch (error) {
+        throw new Error(error);
+    }
+}
