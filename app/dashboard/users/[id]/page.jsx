b91d677cbadb9714d67b6a7c6c73e5c121636a04
Laz <lazgerasimov@gmail.com>
2023-12-05 19:53:29 +0200

    create and update the following using Next 14 server actions: - create update-single-user api call - dynamically render single user details page

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,40 +1,50 @@
+import { updateUser } from '@/app/lib/actions';
+import { fetchUser } from '@/app/lib/data';
 import styles from '@/app/ui/dashboard/users/singleUser.module.css';
 import Image from 'next/image';
 
-const SingleUserPage = () => {
+const SingleUserPage = async ({
+  params
+}) => {
+
+  const { id } = params;
+  const user = await fetchUser(id);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.imgContainer}>
           <Image
-            src="/noavatar.png"
+            src={user.img || "/noavatar.png"}
             alt='no_avatar_image'
             fill
           />
         </div>
-        John Doe
+        {user.username}
       </div>
       <div className={styles.formContainer}>
-        <form action="" className={styles.form}>
+        <form action={updateUser} className={styles.form}>
+          {/* hidden input used to send data using next 14 server actions */}
+          <input type="hidden" name='id' value={user.id} />
           <label>Username</label>
-          <input type="text" name='username' placeholder='John Doe' />
+          <input type="text" name='username' defaultValue={user.username} />
           <label>Email</label>
-          <input type="email" name='email' placeholder='JohnDoe@mail.com' />
+          <input type="email" name='email' defaultValue={user.email} />
           <label>Password</label>
-          <input type="password" name='password' placeholder='*********' />
+          <input type="password" name='password' defaultValue='*********' disabled/>
           <label>Phone</label>
-          <input type="text" name='phone' placeholder='077777777777' />
+          <input type="text" name='phone' defaultValue={user.phone} />
           <label>Address</label>
-          <textarea type="text" name='address' placeholder='Random Test Address' />
+          <textarea type="text" name='address' defaultValue={user.address} />
           <label >Is Admin?</label>
           <select name="isAdmin" id="isAdmin">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value={true} defaultValue={user.isAdmin}>Yes</option>
+            <option value={false} defaultValue={!user.isAdmin}>No</option>
           </select>
           <label >Is Active?</label>
           <select name="isActive" id="isActive">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value={true} defaultValue={user.isActive}>Yes</option>
+            <option value={false} defaultValue={!user.isActive}>No</option>
           </select>
           <button>Update</button>
         </form>
